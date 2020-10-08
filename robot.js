91f27a92ba78eb12ed446e1c762db29becb565f5
John Rykken <john.rykken@gmail.com>
2020-10-07 19:23:29 -0700

    simplify requirements to push a coordinate into the array

--- robot.js
+++ robot.js
@@ -1,21 +1,14 @@
 
 const calculateRobotSafeArea = () => {
-
-  // let point = { id: 0, x: 0, y: 0, safe: true, examined: false }
+  let z = 0 // remove 
   let index = 0 
   let iterate = true
   let points = [{ x: 0, y: 0, safe: true, examined: false }]
-  // let queue = []
-
-  // let masterArray = []
 
   const reducer = (a, c) => parseInt(a) + parseInt(c)
   const safe = (X, Y) => [Math.abs(X).toString().split(''), Math.abs(Y).toString().split('')].flat().reduce(reducer) <= 23
-  const sum = (X, Y) => [Math.abs(X).toString().split(''), Math.abs(Y).toString().split('')].flat().reduce(reducer)
 
   const sortNeighboringPoints = (x, y) => {
- 
-        
     const arr = [
       { x: x, y: y + 1, safe: safe(x, y + 1), examined: false }, // NORTH
       { x: x + 1, y: y, safe: safe(x + 1, y), examined: false }, // EAST 
@@ -23,12 +16,15 @@ const calculateRobotSafeArea = () => {
       { x: x - 1, y: y, safe: safe(x - 1, y), examined: false }, // WEST 
     ]
 
-  
     for (let a of arr) {
       let exists = false 
+      // const exists = points.some(e => e.x === a.x && e.y === a.y) // use this if the reverse for loop is buggy
 
-      // const exists = points.some(e => e.x === a.x && e.y === a.y) // use this if the reverse for loop is buggy 
+      let okayX = Math.sign(a.x) === 1 || Math.sign(a.x) === 0
+      let okayY = Math.sign(a.y) === 1 || Math.sign(a.y) === 0 
+      let staysInQuadrant = okayX && okayY
       
+
       for (let i = points.length - 1; i >= 0; i -= 1) {
         if (!points[i]) break 
         if (points[i].x === a.x && points[i].y === a.y) {
@@ -36,41 +32,53 @@ const calculateRobotSafeArea = () => {
           break
         }
       }
-       
-
-      if (!exists) points.push(a)
-      
+      if (!exists && a.safe && staysInQuadrant) points.push(a)  
     }
-    
+
   }
 
-  let z = 0
   while (iterate) {
-    // console.log('index', index)
     currentPoint = points[index]
-    
-    
-    if (currentPoint.safe && !currentPoint.examined) {
-      console.log('safe')
-      sortNeighboringPoints(currentPoint.x, currentPoint.y)
-    } 
-    else {
-      console.log(`something is wrong. what is current point? ${currentPoint.x}, ${currentPoint.y}: sum: ${sum(currentPoint.x, currentPoint.y)}`)
-
-    } 
-    
-    z += 1
+    console.log(currentPoint) // REMOVE
+    if (currentPoint.safe && !currentPoint.examined) sortNeighboringPoints(currentPoint.x, currentPoint.y)
+    z += 1 // REMOVE
     points[index].examined = true 
-    // console.log('currentPoint', points[index])
     index += 1
-    iterate = z < 500000
-    
+    iterate = points[index] !== undefined // KEEP 
+    // iterate = z < 222
   }
-  // console.log('FINAL INDEX', index)
-  console.log('points', points.reverse()[0])
-  return points.length
+  
+  console.log('points', points.reverse())
+  return points.length * 4
 }
 
 
 
 console.log("final answer", calculateRobotSafeArea()) // 154260
+
+// let x = 0
+// let y = 0
+// let doit = true
+// let points = []
+// const reducer = (a, c) => parseInt(a) + parseInt(c)
+// const sum = (X, Y) => [Math.abs(X).toString().split(''), Math.abs(Y).toString().split('')].flat().reduce(reducer)
+
+// while(doit) {
+//   const SUM = sum(x, y)
+//   console.log(`SUM of X ${x} Y ${y} is ${SUM}`)
+//   points.push([x,y])
+//   doit = y !== 698
+//   y += 1
+// }
+
+// console.log('points', points)
+// console.log('points length', points.length)
+
+// 698 valid points along  X line
+// 698 valid points along -X line
+// 698 valid points along  Y line
+// 698 valid points along -Y line
+// 1   valid point  for    0, 0
+// everything else needs to be calculated 
+
+
